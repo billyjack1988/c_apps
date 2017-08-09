cd1435c663e49c3b8e8f0a12b3b59fe64cfe549c
billyjack1988 <billyjacktattoos@gmail.com>
2017-08-09 12:09:24 -0500

    updated alot of things

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,25 +1,30 @@
-
-
-
-
-
-
 def coin_sorter(cents_received)
 	coins = {}
-	coin_changer = {quarter: 25, dime: 10, nickel: 5, penny: 1}
+	coin_changer = {:quarters => 25, :dimes => 10, :nickel => 5, :pennies => 1}
 	coin_changer.each do |key, amount|
 		if cents_received >= amount
+			if key.to_s == "penny" && amount > 1
+				coins["pennies"] = amount
+					# puts "#{coins} yoooooooooooooooooooooooo looooooooooooooooooooooook"
+		end
 			coins[key] = cents_received / amount
 		end
 		cents_received = cents_received % amount
 	end
 	woop = ""
 	coins.each do |coin,value|
-		woop << "#{value} #{coin}\n"
+	woop << "#{value} #{coin}\n"
 	end
 	"#{woop}"  
-
 end
+
+
+
+
+
+
+
+
 
 
 
